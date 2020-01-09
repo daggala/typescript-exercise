351cc8c0bf05c5fe63fbdcc585aa6b50170db419
Dagny Gudmundsdottir <dagnygudmundsdottir@Dagnys-MacBook-Pro.local>
2020-01-09 20:56:10 +0000

    3rd try publishing media queries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { mediaQueries } from "./utils/mediaQueries";
+import styled from "styled-components";
+
+interface Box {
+  width: Number;
+}
+
+const Box = styled.div`
+  background-color: red;
+  height: 100px;
+  width: 100px;
+  ${mediaQueries("md")`
+    background-color: green;
+  `}
+  ${props => mediaQueries("md")(`width: ${props.width}px`)}
+`;
 
 function App() {
   return (
     <div className="App">
+      <Box width={400} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/utils/mediaQueries.ts
+++ src/utils/mediaQueries.ts
@@ -1,11 +1,80 @@
-const breakpoints = {
+export const breakpoints = {
   sm: 20,
   md: 30,
   lg: 45,
   xl: 60
 };
 
-export const mediaQueries = Object.keys(breakpoints).reduce((acc, key) => {
-  acc[key] = style => `@media (min-width: ${breakpoints[key]}em) { ${style} }`;
-  return acc;
-}, {});
+export const mediaQueries = (key: keyof typeof breakpoints) => {
+  return (style: TemplateStringsArray | String) =>
+    `@media (min-width: ${breakpoints[key]}em) { ${style} }`;
+};
+
+/////////////////////// AS PURE + EDITOR SUGGESTIONS
+
+// const breakpoints = {
+//   sm: 20,
+//   md: 30,
+//   lg: 45,
+//   xl: 60
+// };
+
+// export const mediaQueries = {
+//   sm: style => media(breakpoints.sm)(style),
+//   md: style => media(breakpoints.md)(style),
+//   lg: style => media(breakpoints.lg)(style),
+//   xl: style => media(breakpoints.xl)(style)
+// };
+
+// const media = size => {
+//   return style => `@media (min-width: ${size}em){ ${style}}`;
+// };
+
+///////////////////// THe one i used in the beginning for my blog
+
+// export const oldAlgorithm = Object.keys(breakpoints).reduce((acc, key) => {
+//   acc[key] = style => `@media (min-width: ${breakpoints[key]}em) { ${style} }`;
+//   return acc;
+// }, {});
+
+///////////////////// The one in the comment on Corin'as blog
+
+// export const corina = Object.keys(breakpoints).reduce((acc, cur) => {
+//   acc[cur] = `(min-width: ${breakpoints[cur]}em)`;
+//   return acc;
+// }, {});
+
+// export const mediaQuery = (key: Number) => {
+//   return (style: TemplateStringsArray) =>
+//     `@media (min-width: ${key}em) { ${style} }`;
+// };
+
+// export const breakpoints = {
+
+// }
+
+// export const size = {
+//   mobileS: "520px",
+//   mobileM: "375px",
+//   mobileL: "425px",
+//   tablet: "768px",
+//   laptop: "1024px",
+//   laptopL: "1440px",
+//   desktop: "2560px"
+// };
+
+//  mobileM: (style: TemplateStringsArray) => media(size.mobileM)(style),
+//look if it's possible to take mobileM and and put it for size.MobileM
+
+// export const mediaQueries2 = (key: String) => {
+//   return (style : String) => `@media (min-width: ${breakpoints[key]}em) { ${style} }`;
+// };
+
+// export const mediaQueries = (Object.keys(breakpoints) as Array<keyof typeof breakpoints>).reduce(
+//   (acc, key) => {
+//     acc[key] = (style: String) =>
+//       `@media (min-width: ${breakpoints[key]}em) { ${style} }`;
+//     return acc;
+//   },
+//   {} as { [index: string]: Function }
+// );
